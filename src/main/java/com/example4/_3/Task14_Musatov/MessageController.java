0aa593281ecf696a64e5211ff6ec45c9897f2be5
AndreiMusatov <166915210+AndreiMusatov@users.noreply.github.com>
2025-03-17 14:05:20 +0300

    Add files via upload

---
+++ src/main/java/com/example4/_3/Task14_Musatov/MessageController.java
@@ -0,0 +1,64 @@
+package com.example4._3.Task14_Musatov;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+
+@RestController
+public class MessageController {
+    private List<Message> messages = new ArrayList<>(Arrays.asList(
+            new Message(1, "Сообщение", "Привет, как дела?", LocalDateTime.now())
+    ));
+
+
+    // ПОЛУЧЕНИЕ
+    @GetMapping("/message")
+    public Iterable<Message> getMessages() {
+        return messages;
+    }
+
+    // ПОЛУЧЕНИЕ ПО ПАРАМЕТРУ(ID)
+    @GetMapping("/message/{id}")
+    public Optional<Message> findMessageById(@PathVariable int id) {
+        return messages.stream().filter(m -> m.getId() == id).findFirst();
+    }
+
+    //ДОБАВЛЕНИЕ
+    @PostMapping("/message")
+    public Message addMessage(@RequestBody Message message) {
+        messages.add(message);
+        return message;
+    }
+
+// ОБНОВЛЕНИЕ ПОЗВОЛЯЕТ явно контролировать HTTP-статус код
+
+    @PutMapping("/message/{id}")
+    public ResponseEntity<Message> updateMessage(@PathVariable int id, @RequestBody Message message) {
+        int index = - 1;
+        for (Message m : messages) {
+            if (m.getId() == id) {
+                index = messages.indexOf(m);
+                messages.set(index, message);
+            }
+        }
+        return index == -1
+                ? new ResponseEntity<>(addMessage(message), HttpStatus.CREATED)
+                : new ResponseEntity<>(message, HttpStatus.OK);
+    }
+
+
+    //УДАЛЕНИЕ
+    @DeleteMapping("/message/{id}")
+    public void deleteMessage(@PathVariable int id) {
+        messages.removeIf(m -> m.getId() == id);
+    }
+
+}
